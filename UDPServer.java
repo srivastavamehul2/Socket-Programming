1441be2cb92f7c555933ce0165f1333f51965cd9
srivastavamehul2 <37579857+srivastavamehul2@users.noreply.github.com>
2018-10-27 20:10:09 +0530

    Update UDPServer.java

--- UDPServer.java
+++ UDPServer.java
@@ -9,15 +9,28 @@ public class UDPServer {
 		DataInputStream rfconsole = new DataInputStream(System.in);
 		PrintStream otconsole = new PrintStream(System.out);
 		
-		InetAddress ia = InetAddress.getByName("localhost");
-		otconsole.print("Server: ");
-		String s = rfconsole.readLine();
-		byte[] b = s.getBytes();
-		otconsole.write(b);
-		DatagramPacket dp = new DatagramPacket(b,b.length,ia,3001);
-		soc.send(dp);
 		
-		
+		while(true){
+			otconsole.print("Server: ");
+			String s = rfconsole.readLine();
+			byte[] b = s.getBytes();
+		//	otconsole.write(b);
+			DatagramPacket dp = new DatagramPacket(b,b.length,InetAddress.getByName("localhost"),3001);
+			soc.send(dp);
+			if(s.equals("exit"))
+				break;
+			
+			byte[] b1 = new byte[1000];
+			DatagramPacket dp2 = new DatagramPacket(b1,b1.length);
+			soc.receive(dp2);
+			b = dp2.getData();
+			String s2 = new String(b,0,dp2.getLength());
+			otconsole.println("Client: " + s2);
+			if(s2.equals("exit"))
+				break;
+		}
+			
+		soc.close();
 
 	}
 
