aa333a0b3595ac42fd933c5546b2427ac4e783ef
srivastavamehul2 <srivastavamehul2@gmail.com>
2018-10-27 20:02:03 +0530

    Changes for Two way chat in client

--- UDPClient.java
+++ UDPClient.java
@@ -11,13 +11,27 @@ public class UDPClient {
 		
 		//otconsole.print("Client: ");
 		//String s = rfconsole.readLine();
-		byte[] b = new byte[100];
-		DatagramPacket dp = new DatagramPacket(b,b.length);
-		soc.receive(dp);
-		b = dp.getData();
-		String s = new String(b,0,dp.getLength());
-		otconsole.println(s);
-		
+		while(true){
+			
+			byte[] b = new byte[100];
+			DatagramPacket dp = new DatagramPacket(b,b.length);
+			soc.receive(dp);
+			b = dp.getData();
+			String s = new String(b,0,dp.getLength());
+			otconsole.println("Server: " + s);
+			if(s.equals("exit"))
+				break;
+			
+			otconsole.print("Client: ");
+			String s2 = rfconsole.readLine();
+			b = s2.getBytes();
+			dp = new DatagramPacket(b,b.length,InetAddress.getByName("localhost"),3000);
+			soc.send(dp);
+			if(s2.equals("exit"))
+				break;
+		}
+			
+		soc.close();
 		
 
 	}
